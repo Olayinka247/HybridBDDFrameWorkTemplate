bdd5c304677183daf8d9bb4f8aa7aa5ac4a2c333
Olayinka247 <olayinkasodiq123@gmail.com>
2025-06-24 15:52:27 +0100

    Added Bonigarcia multiple browser driver chrome,firefox and edge, to avoid using the Driver folder holding the .exe file

--- src/test/java/com/qa/base/Base.java
+++ src/test/java/com/qa/base/Base.java
@@ -2,39 +2,92 @@ package com.qa.base;
 
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.qa.util.BrowserFactory;
+//import org.openqa.selenium.chrome.ChromeDriver;
 import com.qa.util.ReadProperties;
 
 /**
  * @author HP ProBook
  * 
- * This class will have common methods to start and close the browser
+ *         This class will have common methods to start and close the browser
  *
  */
+// public class Base {
+//
+// public static WebDriver driver;
+//
+// /**
+// * @return this method will start the browser and start the Applciation
+// */
+// public WebDriver initializeWebDriver() {
+//
+// System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+// driver = new ChromeDriver();
+// driver.manage().timeouts().implicitlyWait(ReadProperties.implicitWaitTime(),
+// TimeUnit.SECONDS);
+// driver.manage().window().maximize();
+// driver.get(ReadProperties.getappURL());
+// return driver;
+//
+// }
+//
+// /**
+// * This method will close the browser
+// */
+//
+// public void closeBrowser() {
+// driver.quit();
+// }
+//
+// }
+
 public class Base {
-	
+
 	public static WebDriver driver;
 
 	/**
-	 * @return this method will start the browser and start the Applciation 
+	 * @return this method will start the browser and launch the Application
 	 */
 	public WebDriver initializeWebDriver() {
+		// Read browser name from system property or default to chrome
+		String browser = System.getProperty("browser", "chrome");
 
-		System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = BrowserFactory.createDriver(browser);
 		driver.manage().timeouts().implicitlyWait(ReadProperties.implicitWaitTime(), TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get(ReadProperties.getappURL());
 		return driver;
-
 	}
 
 	/**
-	 *  This method will close the browser 
+	 * This method will close the browser
 	 */
-	
 	public void closeBrowser() {
-		driver.quit();
+		if (driver != null) {
+			driver.quit();
+		}
 	}
-
 }
+
+//public class Base {
+//    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+//
+//    public static WebDriver getDriver() {
+//        return driver.get();
+//    }
+//
+//    public WebDriver initializeWebDriver(String browser) {
+//        WebDriver localDriver = BrowserFactory.createDriver(browser);
+//        driver.set(localDriver);
+//        getDriver().manage().window().maximize();
+//        getDriver().get(ReadProperties.getappURL());
+//        return getDriver();
+//    }
+//
+//    public void closeBrowser() {
+//        getDriver().quit();
+//        driver.remove();
+//    }
+//}
+
